96d64651529cefb91965853c89e1a25e7eb2eaeb
Rachel Baerts <rachel.bearts.2001@gmail.com>
2021-06-18 15:54:04 +0200

    Portfoliosite code

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// portret changes
+
+function onHoverContact() {
+    document.getElementById("portret").src = "img/homepage/portret_blue.svg";
+}
+
+function onHoverWorks() {
+    document.getElementById("portret").src = "img/homepage/portret_pink.svg";
+}
+
+function onHoverAbout() {
+    document.getElementById("portret").src = "img/homepage/portret_purple.svg";
+}
+
+function onHoverOff() {
+    document.getElementById("portret").src = "img/homepage/portret_purple.svg";
+}
+
+
+
+//menubar
+function myFunction(x) {
+    x.classList.toggle("change");
+}
